d651d0aaf908433d0ff6cdd4938fbf56183102b3
Nikolius Lau <nikolius.lau@koltiva.com>
2023-03-15 22:53:53 +0700

    update bootstrap 5.2

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -12,7 +12,7 @@
 <!-- Core theme CSS (includes Bootstrap)-->
 <link href="{{url('/')}}/css/styles.css" rel="stylesheet" />
 <!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 <script
   src="https://code.jquery.com/jquery-3.6.0.min.js"
